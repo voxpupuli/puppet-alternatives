cc38ef021d783006583ab78272a869b76b3b39be
Adrien Thebo <git@somethingsinistral.net>
2013-05-26 23:41:25 -0700

    Use more general exception in type spec

--- spec/unit/type/alternatives_spec.rb
+++ spec/unit/type/alternatives_spec.rb
@@ -7,7 +7,7 @@ describe Puppet::Type.type(:alternatives) do
       expect { described_class.new(:name => 'ruby', :path => '/usr/bin/ruby1.9') }.to_not raise_error
     end
     it 'fails validation without an absolute path' do
-      expect { described_class.new(:name => 'ruby', :path => "The bees they're in my eyes") }.to raise_error Puppet::ResourceError, /must be a fully qualified path/
+      expect { described_class.new(:name => 'ruby', :path => "The bees they're in my eyes") }.to raise_error Puppet::Error, /must be a fully qualified path/
     end
   end
 end

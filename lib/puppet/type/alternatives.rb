fe595b0acd2734d2257954dcbd08333a95a279c2
Adrien Thebo <git@somethingsinistral.net>
2013-05-26 19:20:54 -0700

    Add alternative type support for `mode` property

--- lib/puppet/type/alternatives.rb
+++ lib/puppet/type/alternatives.rb
@@ -1,5 +1,7 @@
 Puppet::Type.newtype(:alternatives) do
 
+  feature :mode, "The alternative can provide auto and manual modes"
+
   newparam(:name, :isnamevar => true) do
     desc "The name of the alternative."
   end
@@ -9,6 +11,22 @@ Puppet::Type.newtype(:alternatives) do
 
     validate do |path|
       raise ArgumentError, "path must be a fully qualified path" unless absolute_path? path
+    end
+  end
+
+  newproperty(:mode, :required_features => [:mode]) do
+    desc "Use the automatic option for this alternative"
+
+    newvalue('auto')
+    newvalue('manual')
+  end
+
+  validate do
+    case self[:mode]
+    when 'auto'
+      raise ArgumentError, "Mode cannot be 'auto' if a path is given" if self[:path]
+    when 'manual'
+      raise ArgumentError, "Mode cannot be 'manual' without a path" unless self[:path]
     end
   end
 end

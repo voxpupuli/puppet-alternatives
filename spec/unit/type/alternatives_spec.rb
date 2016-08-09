99f5f348e3da8207aa14fc9e9a2aea1bf70a6b58
Tim Meusel <tim@bastelfreak.de>
2016-08-09 09:22:00 +0200

    rubocop: fix RSpec/NotToNot

--- spec/unit/type/alternatives_spec.rb
+++ spec/unit/type/alternatives_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Puppet::Type.type(:alternatives) do
   describe 'property `path`' do
     it 'passes validation with an absolute path' do
-      expect { described_class.new(name: 'ruby', path: '/usr/bin/ruby1.9') }.to_not raise_error
+      expect { described_class.new(name: 'ruby', path: '/usr/bin/ruby1.9') }.not_to raise_error
     end
     it 'fails validation without an absolute path' do
       expect { described_class.new(name: 'ruby', path: "The bees they're in my eyes") }.to raise_error Puppet::Error, %r{must be a fully qualified path}

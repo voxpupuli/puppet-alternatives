9c36e9334145e207b86b4b7f325bdc753d009409
Joshua Hoblitt <josh@hoblitt.com>
2022-07-22 10:11:17 -0700

    add basic acceptance spec

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'voxpupuli/acceptance/spec_helper_acceptance'
+
+configure_beaker
+
+shared_examples 'an idempotent resource' do
+  it 'applies with no errors' do
+    apply_manifest(pp, catch_failures: true)
+  end
+
+  it 'applies a second time without changes' do
+    apply_manifest(pp, catch_changes: true)
+  end
+end
